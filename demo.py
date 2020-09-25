e598f255b17f986cdb80ad4c7e328b1892964b23
amit_srimal <amit.srimal.1901@gmail.com>
2020-09-25 11:55:57 +0530

    List Practice

--- demo.py
+++ demo.py
@@ -1,3 +1,85 @@
+###### STRING###### STRING###### STRING###### STRING###### STRING###### STRING###### STRING###### STRING###### STRING
+#A string is a sequence of characters. Strings are IMMUTABLE, i.e., they cannot be changed.
+##Strings can be updated as WHOLE, but not in PARTIAL(hence called IMMUTABLE)
+#Strings are arrays of bytes representing Unicode characters.
+# #However, Python does not have a character data type, a single character is simply a string with a length of 1.
+# #Square brackets can be used to access elements of the string.
+a = "This is a string"
+print (a)
+type(a) # <class 'str'>
+a[2] # 'i'
+
+## String SLICING
+a[2:8] # 'is is '
+a[:8] # 'This is '
+a[5:7] # 'is'
+
+## Retrieve each element of String
+for i in a:
+    print(i, end="") # This is a string
+    print(i) # each element appears in new line
+
+## Use of Single, Double, Triple quotes
+b1="It's there time now"
+print(b1) ## It's there time now
+b2= 'Sam"s home is here' # Sam"s home is here
+print(b2)
+b3='''Amit 
+        Srimal'''
+print(b3) # Amit Srimal in new line printed here.
+
+## ESCAPE Characters
+## \, \\, \t, \b, \n
+txt1 = "We are the so-called \"Vikings\" from the north."
+print(txt1) ## We are the so-called "Vikings" from the north.
+txt2= 'This has there\'s belonging'
+print(txt2) # This has there's belonging
+txt3= "This will insert one \\ (backslash)."
+print(txt3) # This will insert one \ (backslash).
+
+##To ignore the escape sequences in a String, r or R is used, this implies that the string is a raw string and escape sequences inside it are to be ignored.
+txt4 = "This is \x47\x65\x65\x6b\x73 in \x48\x45\x58"
+print(txt4) ##This is Geeks in HEX : Special characters are recognized and interpraretd
+txt5 = r"This is \x47\x65\x65\x6b\x73 in \x48\x45\x58"
+print(txt5) ## Escape charaters IGNORED and we get This is \x47\x65\x65\x6b\x73 in \x48\x45\x58
+
+
+##UPDATING/ DELETING Entire String (This is different from Immutability concept)
+string1="this is sample string 1"
+string1 = "this is update on sample string 1"
+print(string1)
+# Try deleting few character
+del string1[3:5] # Immutable and TypeError: 'str' object does not support item deletion
+del string1 ## Deletes the string as WHOLE
+
+## FORMATTING
+# Strings in Python can be formatted with the use of format() method
+# Format method in String contains curly braces {} as placeholders which can hold arguments according to position or keyword to specify the order.
+f1 = "{} {}".format('Amit','Srimal')
+f2 = "{1} {0}".format('Srimal','Amit')
+f3 = "{f} {l}".format(l='Srimal', f='Amit')
+print(f1, f2, f3) # Amit Srimal Amit Srimal Amit Srimal
+f4 = "{}+{}".format('Amit','Srimal')
+print(f4) ## Amit+Srimal
+
+## # Formatting of Integers
+f5 = "{0:b}".format(16) ## Binary representation of 16 is 10000
+# Formatting of Floats
+f6 = "{0:e}".format(165.6458)  # Exponent representation of 165.6458 is 1.656458e+02
+# Rounding off Integers
+f7 = "{0:.2f}".format(1/6) ##one-sixth is 0.17 ; upto two float decimals
+f8= "{0:.3f}".format(3.134327429) ## 3.134
+
+
+
+
+
+
+
+
+
+
+
 #################### LIST ###### LIST ###### LIST ###### LIST ###### LIST ###### LIST ###### LIST ###### LIST ###### LIST
 my_list=['Amit',45,'$',56.09,['Srimal',45.7]] ## nested list
 print(my_list) ##['Amit',45,'$',56.09,['Srimal',45.7]]
@@ -66,6 +148,7 @@ def myFunc(e):
   return len(e)
 
 cars = ['Ford', 'Mitsubishi', 'BMW', 'VW']
+print(len(cars[1])) # 10 for Mitsubishi
 cars.sort(reverse=True, key=myFunc)
 print(cars) ## ['Mitsubishi', 'Ford'', 'BMW', 'VW']
 ## without using key
