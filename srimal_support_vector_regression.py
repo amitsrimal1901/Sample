f85d1f7a39b6de6de0827e816cbf5ad55f1c7403
amit_srimal <amit.srimal.1901@gmail.com>
2020-10-15 00:53:43 +0530

    srimal Support Vector Regression Practice

--- srimal_support_vector_regression.py
+++ srimal_support_vector_regression.py
@@ -19,7 +19,15 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y_train = sc_y.fit_transform(y_train)
+
+Alternatively we can Scale first for X, y and then test split it
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+"""
 
 # Fitting the Regression Model to the dataset
 # Create your regressor here
