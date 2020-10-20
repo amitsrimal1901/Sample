4c4fe65eacf9ed2e78ea01c28a0910eeab6f3946
amit_srimal <amit.srimal.1901@gmail.com>
2020-10-20 11:20:12 +0530

    srimal knn classification practice

--- srimal_KNN.py
+++ srimal_KNN.py
@@ -121,9 +121,168 @@ print(classes[y_predict[0]]) # prediction is versicolor
 print(classes[y_predict[1]]) # prediction is setosa
 
 
+#*************************************************************************************************************
+#GITHUB: https://github.com/amitsrimal1901/Machine_Learning_A-Z/blob/master/Part%203%20-%20Classification/Section%2015%20-%20K-Nearest%20Neighbors%20(K-NN)/knn.py
+"""
+K-NN is a non-parametric and lazy learning algorithm. 
+Non-parametric means there is no assumption for underlying data distribution i.e. the model structure determined from the dataset.
+Lazy algorithm because it does not need any training data points for model generation. All training data is used in the testing phase which makes training faster and testing phase slower and costlier.
+K-Nearest Neighbor (K-NN) is a simple algorithm that stores all the available cases and classifies the new data or case based on a similarity measure.
 
+In K-NN classification, the output is a class membership. 
+An object is classified by a plurality vote of its neighbors, with the object being assigned to the class most common among its k nearest neighbors (k is a positive integer, typically small). 
+If k = 1, then the object is simply assigned to the class of that single nearest neighbor.
 
+To determine which of the K instances in the training dataset are most similar to a new input, a distance measure is used.
+For real-valued input variables, the most popular distance measure is the EUCLIDEAN DISTANCE.
 
+##EUCLIDEAN DISTANCE:
+The Euclidean distance is the most common distance metric used in low dimensional data sets. It is also known as the L2 norm. 
+The Euclidean distance is the usual manner in which distance is measured in the real world.
+where p and q are n-dimensional vectors and denoted by p = (p1, p2,…, pn) and q = (q1, q2,…, qn) represent the n attribute values of two records.
+While Euclidean distance is useful in low dimensions, it doesn’t work well in high dimensions and for categorical variables. 
+The drawback of Euclidean distance is that it ignores the similarity between attributes. Each attribute is treated as totally different from all of the attributes.
+
+## Other popular distance measures :
+    1. Hamming Distance: Calculate the distance between binary vectors.
+    2. Manhattan Distance: Calculate the distance between real vectors using the sum of their absolute difference. Also called City Block Distance.
+    3. Minkowski Distance: Generalization of Euclidean and Manhattan distance.
+    
+## STEPS OF ALGO
+    1. Divide the data into training and test data.
+    2. Select a value K.
+    3. Determine which distance function is to be used.
+    4. Choose a sample from the test data that needs to be classified and compute the distance to its n training samples.
+    5. Sort the distances obtained and take the k-nearest data samples.
+    6. Assign the test class to the class based on the majority vote of its k neighbors.    
+
+## Performance of the K-NN algorithm is influenced by three main factors :
+    1. The distance function or distance metric used to determine the nearest neighbors.
+    2. The decision rule used to derive a classification from the K-nearest neighbors.
+    3. The number of neighbors used to classify the new example.
+
+## Advantages of K-NN :
+    1. The K-NN algorithm is very easy to implement.
+    2. Nearly optimal in the large sample limit.
+    3. Uses local information, which can yield highly adaptive behavior.
+    4. Lends itself very easily to parallel implementation.
+    
+## Disadvantages of K-NN :
+    1. Large storage requirements.
+    2. Computationally intensive recall.
+    3. Highly susceptible to the curse of dimensionality.    
+
+## 
+APPLICATION AREA OF KNN:
+    1. Finance — financial institutes will predict the credit rating of customers.
+    2. Healthcare — gene expression.
+    3. Political Science — classifying potential voters in two classes will vote or won’t vote.
+    4. Handwriting detection.
+    5. Image Recognition.
+    6. Video Recognition.
+    7. Pattern Recognition
+
+"""
+#Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('https://raw.githubusercontent.com/amitsrimal1901/Machine_Learning_A-Z/master/Part%203%20-%20Classification/Section%2015%20-%20K-Nearest%20Neighbors%20(K-NN)/Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting K-NN to the Training set
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+#***************************************************************************************************************************
+# https://towardsdatascience.com/knn-algorithm-what-when-why-how-41405c16c36f
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+
+data = pd.read_csv('https://raw.githubusercontent.com/adityakumar529/Coursera_Capstone/master/diabetes.csv')
+
+# There are some factors where the values cannot be zero. Glucose values, for example, cannot be 0 for a human.
+# Similarly, BloodPressure, SkinThickness, Insulin, and BMI cannot be zero for a human.
+
+non_zero = ['Glucose','BloodPressure','SkinThickness','Insulin','BMI']
+for column in non_zero:
+    data[column] = data[column].replace(0,np.NaN)
+    mean = int(data[column].mean(skipna = True))
+    data[column] = data[column].replace(np.NaN, mean)
+    print(data[column])
+
+# sub plotting
+#We have defined non_zero with the column where the values cannot be zero.
+#In each column we will first check if we have 0 values. Then we replace it with NaN.
+#Later we are creating a meaning of the column and replacing the earlier with mean.
+
+import seaborn as sns
+p=sns.pairplot(data, hue = 'Outcome')
+
+# geting fecthing and sample name
 
 
 
